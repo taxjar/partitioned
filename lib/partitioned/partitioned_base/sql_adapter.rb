0fdb4b90015e9cd2ccc4f69f343cc9cfdb2115ed
Corin Schedler <corin@dubdromic.com>
2020-07-15 17:53:29 -0500

    Fix index naming across partitions

--- lib/partitioned/partitioned_base/sql_adapter.rb
+++ lib/partitioned/partitioned_base/sql_adapter.rb
@@ -197,10 +197,13 @@ module Partitioned
       def add_partition_table_index(*partition_key_values)
         configurator.indexes(*partition_key_values).each do |field,options|
           used_options = options.clone
-          unless used_options.has_key?(:name)
+          if !used_options.has_key?(:name)
             name = [*field].join('_')
             used_options[:name] = used_options[:unique] ? unique_index_name(name, *partition_key_values) : index_name(name, *partition_key_values)
+          else
+            used_options[:name] = index_name(used_options[:name], *partition_key_values)
           end
+
           add_index(partition_table_name(*partition_key_values), field, used_options)
         end
       end
